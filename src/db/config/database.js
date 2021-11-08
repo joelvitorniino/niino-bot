3882461bbdd31a8de0f7531f728a7b001923b4db
joelvitorniino <joelprofessor13@gmail.com>
2021-11-08 20:35:44 -0300

    adding dialect sqlite3

--- src/db/config/database.js
+++ src/db/config/database.js
@@ -1,11 +1,7 @@
 require('dotenv').config();
 
 module.exports = {
-    dialect: "mysql",
-    host: process.env.MYSQL_HOST,
-    username: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE,
+    dialect: "sqlite",
     define: {
         timestamps: true,
         underscored: true,
