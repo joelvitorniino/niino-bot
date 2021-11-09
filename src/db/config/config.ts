3814c334ed0fea24f4d2f26f6f9d633376ee76d2
joelvitorniino <joelprofessor13@gmail.com>
2021-11-08 22:49:08 -0300

    new deletions

--- src/db/config/config.ts
+++
@@ -1,9 +0,0 @@
-import { Sequelize } from 'sequelize';
-
-const sequelizeConnection = new Sequelize({
-  dialect: "sqlite",
-  logging: false,
-  storage: "/home/mateus/Documents/Projects/niino-bot/src/db/database.sqlite",
-});
-
-export default sequelizeConnection;
--- src/db/config/database.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config();
-
-module.exports = {
-    dialect: "sqlite",
-    define: {
-        timestamps: true,
-        underscored: true,
-    },
-  };
