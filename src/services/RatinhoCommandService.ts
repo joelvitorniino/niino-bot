5f999ad4ae58ae5a35f577dca374f485496db496
Mateus Henrique <mhenriqq32@gmail.com>
2021-11-11 16:30:52 -0300

    ratinho command change random audio

--- src/services/RatinhoCommandService.ts
+++ src/services/RatinhoCommandService.ts
@@ -1,12 +1,23 @@
 import { Client, Message } from '@open-wa/wa-automate';
 
+const audios = [
+  "http://31.220.20.119/niino-bot/ratinho.mp3",
+  "http://31.220.20.119/niino-bot/uepa.mp3",
+  "http://31.220.20.119/niino-bot/rapaz.mp3",
+  "http://31.220.20.119/niino-bot/esse_e_meu_patrao.mp3",
+  "http://31.220.20.119/niino-bot/pare.mp3",
+  "http://31.220.20.119/niino-bot/cavalo.mp3",
+  "http://31.220.20.119/niino-bot/tarzan.mp3",
+  "http://31.220.20.119/niino-bot/head_shake.mp3",
+  "http://31.220.20.119/niino-bot/isto_non_eczizte.mp3",
+  "http://31.220.20.119/niino-bot/potencia.mp3",
+  "http://31.220.20.119/niino-bot/irra.mp3",
+];
+
 export class RatinhoCommandService {
   async execute(client: Client, message: Message) {
-    await client.sendFileFromUrl(
-      message.from,
-      "http://31.220.20.119/niino-bot/ratinho.mp3",
-      "ratinho.mp3",
-      ""
-    );
+    const randomAudio = audios[Math.round(Math.random() * audios.length)];
+
+    await client.sendFileFromUrl(message.from, randomAudio, "ratinho.mp3", "");
   }
 }
