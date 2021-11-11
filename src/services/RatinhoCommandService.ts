fbc28187152d288637ca3ddc10e28c44985172a3
Mateus Henrique <mhenriqq32@gmail.com>
2021-11-11 15:10:08 -0300

    ratinho command change link

--- src/services/RatinhoCommandService.ts
+++ src/services/RatinhoCommandService.ts
@@ -4,7 +4,7 @@ export class RatinhoCommandService {
   async execute(client: Client, message: Message) {
     await client.sendFileFromUrl(
       message.from,
-      "http://31.220.20.119/ratinho.mp3",
+      "http://31.220.20.119/niino-bot/ratinho.mp3",
       "ratinho.mp3",
       ""
     );
